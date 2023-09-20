0d85a23a4b4d77b1bade07c17c5d90579020bf6f
AndreLp5 <aleppik7@gmail.com>
2023-09-20 11:21:16 +0300

    int to commit

---
+++ l1.py
@@ -0,0 +1,4 @@
+kr = float(input("sisesta kroonid: "))
+eur = kr / 15.6466
+eur = round(eur,2)
+print (eur)
---
+++ ul2.py
@@ -0,0 +1,6 @@
+r = float(input("Input the radius of the circle: "))
+print ("The area of the circle with radius " + str(r)+"is: " + str(3.14159 * r**2))
+area = 3.14159 * r * r
+print ("Area of a circle: ")
+circumference = 2 * 3.14159 * r
+print ("circumference of a circle: ")
