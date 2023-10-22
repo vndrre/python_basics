3a276cafaa6f0ba77d5b855ae307e691b7c6078b
Andre Leppik <145542472+AndreLp5@users.noreply.github.com>
2023-10-22 22:25:15 +0300

    Add files via upload

---
+++ ul3.py
@@ -0,0 +1,5 @@
+n = input("Enter the value :  ")
+nn = n+n
+nnn = n+n+n
+total = int(n) + int(nn) + int(nnn)
+print(n, "+", nn, "+", nnn, "=", total)
---
+++ ul4.py
@@ -0,0 +1,7 @@
+a = input("Sisesta arv 1: ")
+b = input("Sisesta arv 2: ")
+
+if int(a) < int(b):
+    print(a)
+else:
+    print(b)
