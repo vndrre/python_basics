6ec507da5ac3fef2a19735982dd5e6b3f2c929b3
AndreLp5 <aleppik7@gmail.com>
2023-11-20 14:13:31 +0200

    a

---
+++ yl26.py
@@ -0,0 +1,86 @@
+Revenue
+            Johnver Vanston Danbree Vansey  Mundyke
+Tea             190     140    1926     14      143
+Coffee          325      19     293   1491      162
+Water           682      14     852     56      659
+Milk            829     140     609    120       87
+
+
+Johnver = {
+    "Tea": 190,
+    "Coffee": 325,
+    "Water": 682,
+    "Milk": 829
+}
+
+Vanston = {
+    "Tea": 140,
+    "Coffee": 19,
+    "Water": 14,
+    "Milk": 140
+}
+
+Danbree = {
+    "Tea": 1926,
+    "Coffee": 293,
+    "Water": 852,
+    "Milk": 609
+}
+
+Vansey = {
+    "Tea": 14,
+    "Coffee": 1491,
+    "Water": 56,
+    "Milk": 120
+}
+
+Mundyke = {
+    "Tea": 143,
+    "Coffee": 162,
+    "Water": 659,
+    "Milk": 69
+}
+
+
+Expenses
+            Johnver Vanston Danbree Vansey  Mundyke
+Tea             120      65     890     54      430
+Coffee          300      10      23    802      235
+Water            50     299    1290     12      145
+Milk             67     254      89    129       76
+
+
+Johnver2 = {
+    "Tea": 120,
+    "Coffee": 300,
+    "Water": 50,
+    "Milk": 67
+}
+
+Vanston2 = {
+    "Tea": 65,
+    "Coffee": 10,
+    "Water": 299,
+    "Milk": 254
+}
+
+Danbree2 = {
+    "Tea": 890,
+    "Coffee": 23,
+    "Water": 1290,
+    "Milk": 89
+}
+
+Vansey2 = {
+    "Tea": 54,
+    "Coffee": 802,
+    "Water": 12,
+    "Milk": 129
+}
+
+Mundyke2 = {
+    "Tea": 430,
+    "Coffee": 235,
+    "Water": 145,
+    "Milk": 76
+}
